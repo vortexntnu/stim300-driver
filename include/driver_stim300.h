48e30879651c8c99abf806ebf2adfbc2e142db77
AndreasVaage <andrevaa@stud.ntnu.no>
2019-04-01 22:14:42 +0200

    Add complete constructor

--- include/driver_stim300.h
+++ include/driver_stim300.h
@@ -11,52 +11,56 @@
 #include <array>
 #include <vector>
 
-
-
-class DriverStim300 {
+class DriverStim300
+{
 public:
-
-    explicit DriverStim300(SerialDriver &serial_driver);
-    ~DriverStim300();
-    double getAccX() const;
-    double getAccY() const;
-    double getAccZ() const;
-    double getGyroX() const;
-    double getGyroY() const;
-    double getGyroZ() const;
-    uint16_t getLatency_us() const;
-    double getAverageTemp() const;
-    bool isChecksumGood() const;
-    bool isSensorStatusGood() const;
-    uint8_t getInternalMeasurmentCounter() const;
-    bool processPacket();
+  DriverStim300(SerialDriver& serial_driver,
+                stim_300::DatagramIdentifier datagram_id = stim_300::DatagramIdentifier::RATE_ACC_INCL_TEMP_AUX,
+                stim_300::GyroOutputUnit gyro_output_unit = stim_300::GyroOutputUnit::ANGULAR_RATE,
+                stim_300::AccOutputUnit acc_output_unit = stim_300::AccOutputUnit::ACCELERATION,
+                stim_300::InclOutputUnit incl_output_unit = stim_300::InclOutputUnit::ACCELERATION,
+                SerialDriver::BAUDRATE baudrate = SerialDriver::BAUDRATE::BAUD_921600,
+                uint16_t serial_read_timeout_ms = 1);
+  ~DriverStim300();
+  double getAccX() const;
+  double getAccY() const;
+  double getAccZ() const;
+  double getGyroX() const;
+  double getGyroY() const;
+  double getGyroZ() const;
+  uint16_t getLatency_us() const;
+  double getAverageTemp() const;
+  bool isChecksumGood() const;
+  bool isSensorStatusGood() const;
+  uint8_t getInternalMeasurmentCounter() const;
+  bool processPacket();
 
 private:
+  enum class Mode : uint8_t
+  {
+    Init,
+    Normal,
+    Service
+  };
+  Mode mode_;
 
-    enum class Mode: uint8_t{Init, Normal, Service};
-    Mode mode_;
+  SerialDriver& serial_driver_;
+  uint16_t serial_read_timeout_ms_;
+  static constexpr uint8_t BUFFER_SIZE_{ stim_300::MAX_DATAGRAM_SIZE * 2 };
+  std::vector<uint8_t> buffer_;
+  size_t n_read_bytes_;
+  bool in_sync_;
 
-    SerialDriver& serial_driver_;
-    uint16_t serial_read_timeout_ms_;
-    static constexpr uint8_t BUFFER_SIZE_{stim_300::MAX_DATAGRAM_SIZE*2};
-    std::vector<uint8_t > buffer_;
-    size_t n_read_bytes_;
-    bool in_sync_;
+  stim_300::DatagramIdentifier datagram_id_;
+  stim_300::DatagramParser datagram_parser_;
+  uint8_t datagram_size_;
 
+  stim_300::SensorData sensor_data_;
+  bool checksum_is_ok_;
+  bool no_internal_error_;
 
-    stim_300::DatagramIdentifier datagram_id_;
-    stim_300::DatagramParser datagram_parser_;
-    uint8_t datagram_size_;
-
-    stim_300::SensorData sensor_data_;
-    bool checksum_is_ok_;
-    bool no_internal_error_;
-
-
-    uint8_t crc_dummy_bytes_;
-    bool verifyChecksum(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, uint32_t& expected_CRC);
-
+  uint8_t crc_dummy_bytes_;
+  bool verifyChecksum(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, uint32_t& expected_CRC);
 };
 
-
-#endif //DRIVER_STIM300_DRIVER_STIM300_H
+#endif  // DRIVER_STIM300_DRIVER_STIM300_H
--- src/driver_stim300.cpp
+++ src/driver_stim300.cpp
@@ -6,24 +6,23 @@
 #include "driver_stim300.h"
 #include <bitset>
 
-
-DriverStim300::DriverStim300(SerialDriver &serial_driver) :
-    serial_driver_(serial_driver)
-    ,serial_read_timeout_ms_(1)
-    ,mode_(Mode::Normal)
-    ,n_read_bytes_(0)
-    ,in_sync_(false)
-    ,checksum_is_ok_(false)
-    ,no_internal_error_(true)
-    ,crc_dummy_bytes_(stim_300::numberOfPaddingBytes(stim_300::DatagramIdentifier::RATE_ACC_INCL_TEMP_AUX))
-    ,sensor_data_()
-    ,datagram_id_(stim_300::DatagramIdentifier::RATE_ACC_INCL_TEMP_AUX)
-    ,datagram_parser_(stim_300::DatagramIdentifier::RATE_ACC_INCL_TEMP_AUX
-        ,stim_300::GyroOutputUnit::ANGULAR_RATE
-        ,stim_300::AccOutputUnit::ACCELERATION
-        ,stim_300::InclOutputUnit::ACCELERATION)
+DriverStim300::DriverStim300(SerialDriver& serial_driver, stim_300::DatagramIdentifier datagram_id,
+                             stim_300::GyroOutputUnit gyro_output_unit, stim_300::AccOutputUnit acc_output_unit,
+                             stim_300::InclOutputUnit incl_output_unit, SerialDriver::BAUDRATE baudrate,
+                             uint16_t serial_read_timeout_ms)
+  : serial_driver_(serial_driver)
+  , serial_read_timeout_ms_(serial_read_timeout_ms)
+  , datagram_id_(datagram_id)
+  , mode_(Mode::Normal)
+  , n_read_bytes_(0)
+  , in_sync_(false)
+  , checksum_is_ok_(false)
+  , no_internal_error_(true)
+  , crc_dummy_bytes_(stim_300::numberOfPaddingBytes(datagram_id))
+  , sensor_data_()
+  , datagram_parser_(datagram_id, gyro_output_unit, acc_output_unit, incl_output_unit)
 {
-  serial_driver_.open(SerialDriver::BAUDRATE::BAUD_921600);
+  serial_driver_.open(baudrate);
   datagram_size_ = datagram_parser_.getDatagramSize();
 }
 
@@ -32,30 +31,59 @@ DriverStim300::~DriverStim300()
   serial_driver_.close();
 }
 
-double DriverStim300::getAccX()const{return sensor_data_.acc[0];}
-double DriverStim300::getAccY()const{return sensor_data_.acc[1];}
-double DriverStim300::getAccZ()const{return sensor_data_.acc[2];}
-double DriverStim300::getGyroX()const{return sensor_data_.gyro[0];}
-double DriverStim300::getGyroY()const{return sensor_data_.gyro[1];}
-double DriverStim300::getGyroZ()const{return sensor_data_.gyro[2];}
-uint16_t DriverStim300::getLatency_us()const{return sensor_data_.latency_us;}
-bool DriverStim300::isChecksumGood()const{return checksum_is_ok_;}
-bool DriverStim300::isSensorStatusGood()const{return no_internal_error_;}
-uint8_t DriverStim300::getInternalMeasurmentCounter()const{return sensor_data_.counter;}
+double DriverStim300::getAccX() const
+{
+  return sensor_data_.acc[0];
+}
+double DriverStim300::getAccY() const
+{
+  return sensor_data_.acc[1];
+}
+double DriverStim300::getAccZ() const
+{
+  return sensor_data_.acc[2];
+}
+double DriverStim300::getGyroX() const
+{
+  return sensor_data_.gyro[0];
+}
+double DriverStim300::getGyroY() const
+{
+  return sensor_data_.gyro[1];
+}
+double DriverStim300::getGyroZ() const
+{
+  return sensor_data_.gyro[2];
+}
+uint16_t DriverStim300::getLatency_us() const
+{
+  return sensor_data_.latency_us;
+}
+bool DriverStim300::isChecksumGood() const
+{
+  return checksum_is_ok_;
+}
+bool DriverStim300::isSensorStatusGood() const
+{
+  return no_internal_error_;
+}
+uint8_t DriverStim300::getInternalMeasurmentCounter() const
+{
+  return sensor_data_.counter;
+}
 
 double DriverStim300::getAverageTemp() const
 {
-  double sum {0};
-  uint8_t count {0};
+  double sum{ 0 };
+  uint8_t count{ 0 };
 
-  return count != 0 ? sum/count : std::numeric_limits<double>::quiet_NaN();
+  return count != 0 ? sum / count : std::numeric_limits<double>::quiet_NaN();
 }
 
 bool DriverStim300::processPacket()
 {
   if (this->mode_ == Mode::Normal)
   {
-
     // Read from buffer until we find a datagram identifyer,
     // then read the amount of bytes one datagram should contain,
     // then parse that datagram.
@@ -64,7 +92,7 @@ bool DriverStim300::processPacket()
     // begin codesection
 
     uint8_t byte;
-    while (serial_driver_.readByte(byte,serial_read_timeout_ms_))
+    while (serial_driver_.readByte(byte, serial_read_timeout_ms_))
     {
       if (byte == stim_300::datagramIdentifierToRaw(datagram_id_))
       {
@@ -72,35 +100,35 @@ bool DriverStim300::processPacket()
         {
           n_read_bytes_ = 0;
           in_sync_ = true;
-          //std::cout<<"In sync"<<std::endl;
+          // std::cout<<"In sync"<<std::endl;
         }
         else if (not in_sync_)
         {
           in_sync_ = true;
-          //std::cout<<"Initialised sync"<<std::endl;
+          // std::cout<<"Initialised sync"<<std::endl;
           n_read_bytes_ = 0;
         }
         else
         {
-          //std::cout<<"Random byte is equal datagram id"<<std::endl;
+          // std::cout<<"Random byte is equal datagram id"<<std::endl;
         }
       }
 
       buffer_.push_back(byte);
       n_read_bytes_++;
-      if (buffer_.size()> datagram_size_)
+      if (buffer_.size() > datagram_size_)
         buffer_.erase(buffer_.begin());
 
-      if(n_read_bytes_ == datagram_size_)
+      if (n_read_bytes_ == datagram_size_)
         break;
     }
-    if(buffer_.empty())
+    if (buffer_.empty())
     {
-      //std::cout << "Empty buffer" << std::endl;
+      // std::cout << "Empty buffer" << std::endl;
       return false;
     }
 
-    //std::cout<<"N read bytes: "<<n_read_bytes_<<std::endl;
+    // std::cout<<"N read bytes: "<<n_read_bytes_<<std::endl;
     auto begin = buffer_.begin();
     auto it = std::next(begin);
 
@@ -113,34 +141,33 @@ bool DriverStim300::processPacket()
 
     no_internal_error_ = datagram_parser_.parseDatagram(it, sensor_data_);
 
-    checksum_is_ok_ = verifyChecksum(begin,it,sensor_data_.crc);
+    checksum_is_ok_ = verifyChecksum(begin, it, sensor_data_.crc);
 
     return true;
-
   }
   else if (this->mode_ == Mode::Service)
   {
     std::string s(buffer_.begin(), buffer_.end());
-    std::cout<<s<<"\n";
+    std::cout << s << "\n";
     return true;
   }
 
   return false;
 }
 
-bool DriverStim300::verifyChecksum(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end, uint32_t& expected_CRC)
+bool DriverStim300::verifyChecksum(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end,
+                                   uint32_t& expected_CRC)
 {
-  assert(datagram_size_ == (end-begin));
-  boost::crc_basic<32>  crc_32_calculator(0x04C11DB7, 0xFFFFFFFF, 0x00, false, false);
-  uint8_t buffer_CRC[datagram_size_-sizeof(uint32_t)+crc_dummy_bytes_];
-  std::copy (begin, end-sizeof(uint32_t)+crc_dummy_bytes_, buffer_CRC);
+  assert(datagram_size_ == (end - begin));
+  boost::crc_basic<32> crc_32_calculator(0x04C11DB7, 0xFFFFFFFF, 0x00, false, false);
+  uint8_t buffer_CRC[datagram_size_ - sizeof(uint32_t) + crc_dummy_bytes_];
+  std::copy(begin, end - sizeof(uint32_t) + crc_dummy_bytes_, buffer_CRC);
 
   /** Fill the Dummy bytes with 0x00. There are at the end of the buffer **/
-  for (size_t i=0; i<crc_dummy_bytes_; ++i)
-    buffer_CRC[sizeof(buffer_CRC)-(1+i)] = 0x00;
+  for (size_t i = 0; i < crc_dummy_bytes_; ++i)
+    buffer_CRC[sizeof(buffer_CRC) - (1 + i)] = 0x00;
 
   crc_32_calculator.process_bytes(buffer_CRC, sizeof(buffer_CRC));
 
   return crc_32_calculator.checksum() == expected_CRC;
 }
-
