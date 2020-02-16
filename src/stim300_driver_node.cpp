cac5e7e1723a6a42f570f55e11168a1ec497b73d
oyvind1501 <oyvind1401@hotmail.com>
2020-02-16 15:40:49 +0100

    Add calibration node

--- src/stim300_driver_node.cpp
+++ src/stim300_driver_node.cpp
@@ -1,6 +1,6 @@
 #include "driver_stim300.h"
 #include "serial_unix.h"
-
+#include "math.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 #include "std_srvs/Trigger.h"
@@ -8,6 +8,7 @@
 
 
 bool calibration_mode{false};
+constexpr int NUMBER_OF_CALIBRATION_SAMPLES{1000};
 
 
 
@@ -25,7 +26,6 @@ bool responseCalibrateIMU(std_srvs::Trigger::Request &calibration_request, std_s
 }
 
 
-
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "stim300_driver_node");
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
   double gravity{ 0 };
 
   node.param<std::string>("device_name", device_name, "/dev/ttyUSB0");
-  node.param("variance_gyro", variance_gyro,0.0001);
+  node.param("variance_gyro", variance_gyro,0.0001*2*4.6*pow(10,-4));
   node.param("variance_acc", variance_acc, 4.0);
   node.param("sample_rate", sample_rate, 125);
   node.param("gravity", gravity, 9.80665);
@@ -61,6 +61,7 @@ int main(int argc, char** argv)
 
   ros::ServiceServer service = node.advertiseService("IMU_calibration",responseCalibrateIMU);
 
+
   // New messages are sent from the sensor with sample_rate
   // As loop_rate determines how often we check for new data
   // on the serial buffer, theoretically loop_rate = sample_rate
@@ -72,8 +73,27 @@ int main(int argc, char** argv)
     SerialUnix serial_driver(device_name, stim_const::BaudRate::BAUD_921600);
     DriverStim300 driver_stim300(serial_driver);
 
-
     ROS_INFO("STIM300 IMU driver initialized successfully");
+
+    int difference_in_dataGram{0};
+    int count_messages{0};
+    int number_of_samples{0};
+    double inclination_x{0};
+    double inclination_y{0};
+    double inclination_z{0};
+
+    double average_calibration_roll{0};
+    double average_calibration_pitch{0};
+    double inclination_x_calibration_sum{0};
+    double inclination_y_calibration_sum{0};
+    double inclination_z_calibration_sum{0};
+    double inclination_x_average{0};
+    double inclination_y_average{0};
+    double inclination_z_average{0};
+    inclination_x = driver_stim300.getIncX();
+    inclination_y = driver_stim300.getIncY();
+    inclination_z = driver_stim300.getIncZ();
+
 
     while (ros::ok())
     {
@@ -84,15 +104,45 @@ int main(int argc, char** argv)
         case Stim300Status::OUTSIDE_OPERATING_CONDITIONS:
           ROS_DEBUG("Stim 300 outside operating conditions");
         case Stim300Status::NEW_MEASURMENT:
-          stim300msg.header.stamp = ros::Time::now();
-          stim300msg.linear_acceleration.x = driver_stim300.getAccX() * gravity;
-          stim300msg.linear_acceleration.y = driver_stim300.getAccY() * gravity;
-          stim300msg.linear_acceleration.z = driver_stim300.getAccZ() * gravity;
-          stim300msg.angular_velocity.x = driver_stim300.getGyroX();
-          stim300msg.angular_velocity.y = driver_stim300.getGyroY();
-          stim300msg.angular_velocity.z = driver_stim300.getGyroZ();
-          imuSensorPublisher.publish(stim300msg);
-          break;
+           if (calibration_mode == true)
+            {
+                if(number_of_samples < NUMBER_OF_CALIBRATION_SAMPLES)
+                {
+                    
+                    inclination_x_calibration_sum += inclination_x;
+                    inclination_y_calibration_sum += inclination_y;
+                    inclination_z_calibration_sum += inclination_z;
+
+                }
+                else
+                {
+                    
+                    inclination_x_average = inclination_x_calibration_sum/NUMBER_OF_CALIBRATION_SAMPLES;
+                    inclination_y_average = inclination_y_calibration_sum/NUMBER_OF_CALIBRATION_SAMPLES;
+                    inclination_z_average = inclination_z_calibration_sum/NUMBER_OF_CALIBRATION_SAMPLES;
+
+                    average_calibration_roll = atan2(inclination_y_average,inclination_z_average);
+                    average_calibration_pitch = atan2(-inclination_x_average,sqrt(pow(inclination_y_average,2)+pow(inclination_z_average,2)));
+                    
+                    ROS_INFO("roll: %f", average_calibration_roll);
+                    ROS_INFO("pitch: %f", average_calibration_pitch);
+                    ROS_INFO("IMU Calibrated");
+                    calibration_mode = false;
+                }
+              break;  
+            }
+            else
+            {
+                    stim300msg.header.stamp = ros::Time::now();
+                    stim300msg.linear_acceleration.x = driver_stim300.getAccX() * gravity;
+                    stim300msg.linear_acceleration.y = driver_stim300.getAccY() * gravity;
+                    stim300msg.linear_acceleration.z = driver_stim300.getAccZ() * gravity;
+                    stim300msg.angular_velocity.x = driver_stim300.getGyroX();
+                    stim300msg.angular_velocity.y = driver_stim300.getGyroY();
+                    stim300msg.angular_velocity.z = driver_stim300.getGyroZ();
+                    imuSensorPublisher.publish(stim300msg);
+                    break;
+            }
         case Stim300Status::CONFIG_CHANGED:
           ROS_INFO("Updated Stim 300 imu config: ");
           ROS_INFO("%s", driver_stim300.printSensorConfig().c_str());
